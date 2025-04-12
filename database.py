4ef6aa763a68731590a8d5fd6fb5ca7eb1aa22f0
Srikardrdo2026 <saisrikarsharma2005@gmail.com>
2025-04-12 20:32:03 +0530

    database.py

--- database.py
+++ database.py
@@ -1,18 +1,9 @@
 from pymongo import MongoClient
+from config import Congig
 import os
 
-# Load MongoDB credentials from environment variables
-MONGO_USER = os.getenv("MONGO_USER", "Srikar")  # Default user
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "One_Piece")  # Default password
-MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
-MONGO_PORT = os.getenv("MONGO_PORT", "27017")
-MONGO_DB = os.getenv("MONGO_DB", "expense_tracker")
-
-# Construct MongoDB URI with authentication
-MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource=admin"
-
 # Establish MongoDB connection
-client = MongoClient(MONGO_URI)
+client = MongoClient(Config.MONGO_URI)
 db = client[MONGO_DB]
 
 # Collections
